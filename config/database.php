a705ccd1363539849239c8d095ff72eb320c3114
Oluwatobi Odekunle <ooluwatobialao@gmail.com>
2022-05-02 18:48:25 +0100

    database updated

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DATABASE_URL', "db4free.net"),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'larapay'),
+            'username' => env('DB_USERNAME', 'teecode'),
+            'password' => env('DB_PASSWORD', 'lordteecode'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
